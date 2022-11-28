8a83a32d29c29147e5c7da88f1aac2b73393e928
dbsizu <116579652+dbsizu@users.noreply.github.com>
2022-11-28 17:44:55 +0000

    Second commit

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Paul!</p>
+                <p className="text-center">Izu's app!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
