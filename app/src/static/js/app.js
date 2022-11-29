63d8ed89e7a2920266007e3707b4606bb372df20
dbsizu <116579652+dbsizu@users.noreply.github.com>
2022-11-29 14:54:24 +0000

    Update app

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Izu's app!</p>
+                <p className="text-center">Izu's test app!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
